afe0e4b9d647075e8e741d530953204c05e83f97
Aluisco Miguel Ricardo Mastrapa <aluisco2005@gmail.com>
2023-01-09 16:42:18 -0500

    Modify Error message by default

--- server.js
+++ server.js
@@ -6,3 +6,8 @@ fs.readFile('./data/starter.txt', 'utf-8', (err, data) =>{
     if (err) throw err;
     console.log(data);
 })
+
+process.on('uncaughtException', err => {
+    console.error(`There was an uncaught error: ${err}`);
+    process.exit(1);
+})
